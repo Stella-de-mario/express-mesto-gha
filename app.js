16235645dcd8a43164d0265a155b1109f6d3f395
Stella-de-mario <irina17otr@gmail.com>
2022-09-08 22:24:42 +0500

    14.3

--- app.js
+++ app.js
@@ -11,18 +11,19 @@ const NotFoundError = require('./utils/errors/NotFoundError');
 const { PORT = 3000 } = process.env;
 const app = express();
 
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: false,
+});
+app.use(cookieParser());
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
-
+app.use(auth);
 app.post('/signin', validateLogin, login);
 app.post('signup', validateCreateUser, createUser);
-
 app.use('/cards', require('./routes/cards'));
 app.use('/users', require('./routes/users'));
-
-app.use(auth);
 
 app.use('/*', (req, res) => res.status(NotFoundError).send({ message: 'Запрашиваемая страница не найдена' }));
 
@@ -33,14 +34,6 @@ app.use((err, req, res, next) => {
 });
 app.use(errors());
 
-async function main() {
-  await mongoose.connect('mongodb://localhost:27017/mestodb', {
-    useNewUrlParser: true,
-    useUnifiedTopology: false,
-  });
-  await app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`);
-  });
-}
-
-main();
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
--- utils/constants.js
+++ utils/constants.js
@@ -2,19 +2,6 @@ const { celebrate, Joi } = require('celebrate');
 
 const regexUrl = /^https?:\/\/(www\.)?[a-zA-Z0-9]+\.[-._~:/?#[\]@!$&'()*+,;=]{2,}?#?$/;
 
-const validateGetCardId = celebrate({
-  params: Joi.object().keys({
-    cardId: Joi.string().required().hex().length(24),
-  }),
-});
-
-const validateCreateCard = celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required().pattern(regexUrl),
-  }),
-});
-
 const validateLogin = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
@@ -51,13 +38,26 @@ const validateUpdateAvatar = celebrate({
   }),
 });
 
+const validateGetCardId = celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().alphanum().length(24),
+  }),
+});
+
+const validateCreateCard = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().pattern(regexUrl),
+  }),
+});
+
 module.exports = {
   regexUrl,
-  validateGetCardId,
-  validateCreateCard,
   validateLogin,
   validateCreateUser,
   validateGetUserId,
   validateUpdateUser,
   validateUpdateAvatar,
+  validateGetCardId,
+  validateCreateCard,
 };
