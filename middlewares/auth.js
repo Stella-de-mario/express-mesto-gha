118580aa6a1a884d502f935e696665e25352716e
Stella-de-mario <irina17otr@gmail.com>
2022-09-10 00:08:13 +0500

    14.2

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,18 +3,14 @@ const UnauthorizedError = require('../utils/errors/UnauthorizedError');
 
 const auth = (req, res, next) => {
   const token = req.cookies.jwt;
-  if (!token) {
+  let payload;
+  try {
+    payload = jwt.verify(token, 'SECRET');
+  } catch (err) {
     next(new UnauthorizedError('Пожалуйста, авторизуйтесь'));
-  } else {
-    let payload;
-    try {
-      payload = jwt.verify(token, 'SECRET');
-    } catch (err) {
-      next(new UnauthorizedError('Пожалуйста, авторизуйтесь'));
-    }
-    req.user = payload;
-    next();
   }
+  req.user = payload;
+  next();
 };
 
 module.exports = auth;
